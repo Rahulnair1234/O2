8bc8634082cc023ed3a63114fe9ca23ee65d8813
Atharv Lingayat <76042372+atharv2608@users.noreply.github.com>
2022-12-05 10:29:31 +0530

    PA g form links added

---
+++ scripts/formsredirecting.js
@@ -0,0 +1,54 @@
+
+// Performing Arts
+function ayeGanpat(){
+    window.open(
+        'https://forms.gle/HDbqbRnCumfucbrT9',
+        '_blank'
+      );
+}
+
+function bootsAndCats(){
+    window.open(
+        'https://forms.gle/yiQikFwdekaX1agx7',
+        '_blank'
+      );
+}
+
+function humAurTum(){
+    window.open(
+        'https://forms.gle/MNjVwr8jxHzN2t4V7',
+        '_blank'
+      );
+}
+
+function kaafiKool(){
+    window.open(
+        'https://forms.gle/FzK3cjH2e4RcGiyXA',
+        '_blank'
+      );
+}
+
+function soundOkBandPlease(){
+    window.open(
+        'https://forms.gle/A8NFp8GnK5NUCP2k8',
+        '_blank'
+      );
+}
+
+function chotaMoohBadiBaat(){
+    window.open(
+        'https://forms.gle/Y8B13RvLceibqTCfA',
+        '_blank'
+      );
+}
+
+function nachNaJane(){
+    window.open(
+        'https://forms.gle/sAspum8LPbr2PKrn9',
+        '_blank'
+      );
+}
+
+
+
+
