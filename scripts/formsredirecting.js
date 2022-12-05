a5c135bf9c87cfccae7b9c4958a09520da45c765
Atharv Lingayat <76042372+atharv2608@users.noreply.github.com>
2022-12-05 13:58:49 +0530

    Theme Changed

--- scripts/formsredirecting.js
+++ scripts/formsredirecting.js
@@ -50,5 +50,201 @@ function nachNaJane(){
 }
 
 
+//Fine Arts
+
+function dekhoMagarPyaarSe(){
+    window.open(
+        'https://forms.gle/kWctHZKPC7zXsGyx6',
+        '_blank'
+      );
+}
+
+function futureBrightHei(){
+    window.open(
+        'https://forms.gle/r1AASapxYWaKj45s8',
+        '_blank'
+      );
+}
+
+function ohHasinaJulfoVali(){
+    window.open(
+        'https://forms.gle/4k7q4CU78LVFypCB7',
+        '_blank'
+      );
+}
+
+function rangDeManch(){
+    window.open(
+        'https://forms.gle/dDCtqZJhKzXK1pMA8',
+        '_blank'
+      );
+}
+
+function koyleKaKamaal(){
+    window.open(
+        'https://forms.gle/cgwWV1H49x4E32Pm6',
+        '_blank'
+      );
+}
+
+function digitalCreativiry(){
+    window.open(
+        'https://forms.gle/ujkeHwTJFsRxZyCE6',
+        '_blank'
+      );
+}
+
+function retKiKala(){
+    window.open(
+        'https://forms.gle/69Gkvk6HCcuv4uu1A',
+        '_blank'
+      );
+}
+
+//Literary Arts
+function bazigar(){
+    window.open(
+        'https://forms.gle/myfKNehMda2hmFyp9',
+        '_blank'
+      );
+}
+function kalpanikMun(){
+    window.open(
+        'https://forms.gle/meAhBz9yGGHgEYMm6',
+        '_blank'
+      );
+}
+function magajmaari(){
+    window.open(
+        'https://forms.gle/t4dckcskcVqhQpbv9',
+        '_blank'
+      );
+}
+function minuteMaster(){
+    window.open(
+        'https://forms.gle/Sk385mGizWZ8UPHz6',
+        '_blank'
+      );
+}
+function seeThrough(){
+    window.open(
+        'https://forms.gle/RS55wcUvBscptnEm7',
+        '_blank'
+      );
+}
+function tedhaHeinParMeraHein(){
+    window.open(
+        'https://forms.gle/EYv7PvGw5xPfV9pX9',
+        '_blank'
+      );
+}
 
 
+function hatapai(){
+  window.open(
+    'https://forms.gle/pozPf2PqFtLzHRLo6',
+    '_blank'
+  )
+}
+
+function bearBiceps(){
+  window.open(
+    'https://forms.gle/PQkQJjeMN5riehod6',
+    '_blank'
+  )
+}
+
+function cricket(){
+  window.open(
+    'https://forms.gle/nYyH84pZGnwkBLeV6',
+    '_blank'
+  )
+}
+
+function football(){
+  window.open(
+    'https://forms.gle/jWgn6DT2iqJLKpJ18',
+    '_blank'
+  )
+}
+
+function khokho(){
+  window.open(
+    'https://forms.gle/TxcCWHRRU8NBYA3U9',
+    '_blank'
+  )
+}
+
+function kheechKeMaar(){
+  window.open(
+    'https://forms.gle/3c2iPXNPJGzyRrVu9',
+    '_blank'
+  )
+}
+
+function thrilling(){
+  window.open(
+    'https://forms.gle/MSagwkJBAG4HAbUH7',
+    '_blank'
+  )
+}
+
+
+//Media Management
+
+function hisabKitab(){
+  window.open(
+    'https://forms.gle/Ch7k276WHqJFLCLm6',
+    '_blank'
+  )
+}
+
+function bachaSakoTohBachaLo(){
+  window.open(
+    'https://forms.gle/7SMTCtHj433ojq538',
+    '_blank'
+  )
+}
+
+function bechkeDikhao(){
+  window.open(
+    'https://forms.gle/F23bBQSHaVyJEago6',
+    '_blank'
+  )
+}
+function kimatLikesKi(){
+  window.open(
+    'https://forms.gle/zbQL3Uf7Q2VRDXgw6',
+    '_blank'
+  )
+}
+function zindagiEkSafar(){
+  window.open(
+    'https://forms.gle/EKtu9Gh1pdftoHoaA',
+    '_blank'
+  )
+}
+
+
+// Informals
+
+function bhardoJholi(){
+  window.open(
+    'https://forms.gle/kfxPpzEFpbSPJoVM8',
+    '_blank'
+  )
+}
+
+function kiskaRaj(){
+  window.open(
+    'https://forms.gle/Wpoy1zKaraV6ueaX6',
+    '_blank'
+  )
+}
+
+function treasureHunt(){
+  window.open(
+    'https://forms.gle/yscZ16jchJjmvZi98',
+    '_blank'
+  )
+}
